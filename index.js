aacae3c3ef5fc7b116fda3b2f2b7267fc88ac274
Lucas Loura <oliverloura2020@gmail.com>
2023-04-05 12:43:00 -0300

    updating

--- index.js
+++ index.js
@@ -19,9 +19,40 @@ function mascara(i){
     i.setAttribute("maxlength", "14");
     if (v.length == 3 || v.length == 7) i.value += ".";
     if (v.length == 11) i.value += "-";
- 
- }
+}
 
  function logarCPF(){
-    alert("to be continued");
- }
+    var x = document.getElementById("txtCpf").value;
+    var y = document.getElementById("cpfInvalido");
+
+    y.style.color = "white";
+
+    if(x.length == 14){
+        if (x == "000.000.000-00")
+            window.open('./recadastro/index.html', '_self');
+        else 
+            window.open('./cadastro/index.html', '_self');
+    }
+    else
+        y.style.color = "red";
+}
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
